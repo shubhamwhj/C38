a0876857dec3bd7c1ab19c99cfb1f3d554482a41
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-03-03 03:24:57 +0530

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -19,17 +19,18 @@ class Player {
   }
 
   update(){
-    var playerIndex = "players/player" + this.index;
-    database.ref(playerIndex).set({
+    var playerIndex = "player/players" + this.index;
+    database.ref(playerIndex).update({
       name:this.name,
       distance:this.distance
     });
   }
 
-  static getPlayerInfo(){
+   getPlayerInfo(){
     var playerInfoRef = database.ref('players');
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
     })
+     console.log(allPlayers);
   }
 }
